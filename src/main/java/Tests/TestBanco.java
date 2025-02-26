6772343341f2201ed79238fc1397ae47521ceba0
Nelson Gonzalez Londoño <natanielvideos@gmail.com>
2025-02-25 23:33:37 -0500

    Test Final

--- src/main/java/Tests/TestBanco.java
+++ src/main/java/Tests/TestBanco.java
@@ -87,39 +87,132 @@ public class TestBanco {
         banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.JANUARY, 1, 10, 10, 30), "prueba", 200, moli);
         banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.JANUARY, 3, 10, 10, 30), "muestra", 200, loim);
         banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.JANUARY, 8, 10, 10, 30), "prueba", 400, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.FEBRUARY, 10, 10, 10, 30), "prueba", 200, loim);banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.FEBRUARY, 18, 10, 10, 30), "muestra", 600, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.FEBRUARY, 24, 10, 10, 30), "muestra", 400, moli);banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.MARCH, 4, 10, 10, 30), "prueba", 200, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.FEBRUARY, 10, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.FEBRUARY, 18, 10, 10, 30), "muestra", 600, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.FEBRUARY, 24, 10, 10, 30), "muestra", 400, moli);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.MARCH, 4, 10, 10, 30), "prueba", 200, moli);
         banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.MARCH, 7, 10, 10, 30), "muestra", 400, moli);
         banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.MARCH, 24, 10, 10, 30), "prueba", 600, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.APRIL, 7, 10, 10, 30), "muestra", 200, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.APRIL, 11, 10, 10, 30), "prueba", 600, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.APRIL, 23, 10, 10, 30), "prueba", 200, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.MAY, 12, 10, 10, 30), "muestra", 600, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.MAY, 20, 10, 10, 30), "muestra", 800, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.MAY, 24, 10, 10, 30), "muestra", 400, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.JUNE, 18, 10, 10, 30), "prueba", 800, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.JUNE, 18, 10, 10, 30), "muestra", 600, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.JUNE, 18, 10, 10, 30), "prueba", 200, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.JULY, 23, 10, 10, 30), "prueba", 200, moli);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.JULY, 23, 10, 10, 30), "muestra", 800, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.JULY, 23, 10, 10, 30), "prueba", 200, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.AUGUST, 24, 10, 10, 30), "muestra", 400, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.AUGUST, 24, 10, 10, 30), "muestra", 600, moli);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.AUGUST, 24, 10, 10, 30), "prueba", 400, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.SEPTEMBER, 6, 10, 10, 30), "muestra", 400, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.SEPTEMBER, 6, 10, 10, 30), "prueba", 200, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.SEPTEMBER, 6, 10, 10, 30), "muestra", 800, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.OCTOBER, 3, 10, 10, 30), "prueba", 600, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.OCTOBER, 3, 10, 10, 30), "prueba", 200, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.OCTOBER, 3, 10, 10, 30), "prueba", 400, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.NOVEMBER, 7, 10, 10, 30), "muestra", 400, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.NOVEMBER, 7, 10, 10, 30), "prueba", 200, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.NOVEMBER, 7, 10, 10, 30), "muestra", 400, loim);
-        banco.RealizarTransaccion(loim, LocalDateTime.of(2014, Month.DECEMBER, 9, 10, 10, 30), "muestra", 600, moli);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.DECEMBER, 9, 10, 10, 30), "prueba", 200, loim);
-        banco.RealizarTransaccion(moli, LocalDateTime.of(2014, Month.DECEMBER, 9, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.APRIL, 7, 10, 10, 30), "muestra", 200, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.APRIL, 11, 10, 10, 30), "prueba", 600, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.APRIL, 23, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.MAY, 12, 10, 10, 30), "muestra", 600, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.MAY, 20, 10, 10, 30), "muestra", 800, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.MAY, 24, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.JUNE, 2, 10, 10, 30), "prueba", 800, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.JUNE, 18, 10, 10, 30), "muestra", 600, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.JUNE, 28, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.JULY, 3, 10, 10, 30), "prueba", 200, moli);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.JULY, 13, 10, 10, 30), "muestra", 800, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.JULY, 23, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.AUGUST, 4, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.AUGUST, 24, 10, 10, 30), "muestra", 600, moli);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.AUGUST, 25, 10, 10, 30), "prueba", 400, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.SEPTEMBER, 6, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.SEPTEMBER, 16, 10, 10, 30), "prueba", 200, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.SEPTEMBER, 22, 10, 10, 30), "muestra", 800, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.OCTOBER, 3, 10, 10, 30), "prueba", 600, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.OCTOBER, 12, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.OCTOBER, 30, 10, 10, 30), "prueba", 400, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.NOVEMBER, 7, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.NOVEMBER, 14, 10, 10, 30), "prueba", 200, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.NOVEMBER, 27, 10, 10, 30), "muestra", 400, loim);
+        banco.RealizarTransaccion(loim, LocalDateTime.of(2024, Month.DECEMBER, 9, 10, 10, 30), "muestra", 600, moli);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.DECEMBER, 11, 10, 10, 30), "prueba", 200, loim);
+        banco.RealizarTransaccion(moli, LocalDateTime.of(2024, Month.DECEMBER, 22, 10, 10, 30), "muestra", 400, loim);
         String factura = banco.obtenerPorcentajeGastosIngresos("123","1234",LocalDateTime.of(2024, Month.JANUARY, 1, 1, 0, 0),LocalDateTime.of(2025, Month.JANUARY, 1, 23, 59, 59));
-        System.out.println(factura);
+        String mensaje = "Fecha inicio: 2024-01-01T01:00 - Fecha fin: 2024-02-01T01:00\n" +
+                "% Gastos del mes: 9.803922%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 2.5%\n" +
+                "0.0% Ingresos de la categoria: prueba\n" +
+                "100.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-02-01T01:00 - Fecha fin: 2024-03-01T01:00\n" +
+                "% Gastos del mes: 5.882353%\n" +
+                "0.0% Gastos de la categoria: prueba\n" +
+                "100.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 10.0%\n" +
+                "25.0% Ingresos de la categoria: prueba\n" +
+                "75.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-03-01T01:00 - Fecha fin: 2024-04-01T01:00\n" +
+                "% Gastos del mes: 9.803922%\n" +
+                "40.0% Gastos de la categoria: prueba\n" +
+                "60.000004% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 7.5000005%\n" +
+                "100.0% Ingresos de la categoria: prueba\n" +
+                "0.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-04-01T01:00 - Fecha fin: 2024-05-01T01:00\n" +
+                "% Gastos del mes: 7.8431377%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 5.0%\n" +
+                "50.0% Ingresos de la categoria: prueba\n" +
+                "50.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-05-01T01:00 - Fecha fin: 2024-06-01T01:00\n" +
+                "% Gastos del mes: 7.8431377%\n" +
+                "0.0% Gastos de la categoria: prueba\n" +
+                "100.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 15.000001%\n" +
+                "0.0% Ingresos de la categoria: prueba\n" +
+                "100.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-06-01T01:00 - Fecha fin: 2024-07-01T01:00\n" +
+                "% Gastos del mes: 9.803922%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 10.0%\n" +
+                "25.0% Ingresos de la categoria: prueba\n" +
+                "75.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-07-01T01:00 - Fecha fin: 2024-08-01T01:00\n" +
+                "% Gastos del mes: 13.725491%\n" +
+                "28.57143% Gastos de la categoria: prueba\n" +
+                "71.42857% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 2.5%\n" +
+                "100.0% Ingresos de la categoria: prueba\n" +
+                "0.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-08-01T01:00 - Fecha fin: 2024-09-01T01:00\n" +
+                "% Gastos del mes: 13.725491%\n" +
+                "42.857143% Gastos de la categoria: prueba\n" +
+                "57.14286% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 5.0%\n" +
+                "0.0% Ingresos de la categoria: prueba\n" +
+                "100.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-09-01T01:00 - Fecha fin: 2024-10-01T01:00\n" +
+                "% Gastos del mes: 3.9215689%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 15.000001%\n" +
+                "0.0% Ingresos de la categoria: prueba\n" +
+                "100.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-10-01T01:00 - Fecha fin: 2024-11-01T01:00\n" +
+                "% Gastos del mes: 5.882353%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 10.0%\n" +
+                "100.0% Ingresos de la categoria: prueba\n" +
+                "0.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-11-01T01:00 - Fecha fin: 2024-12-01T01:00\n" +
+                "% Gastos del mes: 3.9215689%\n" +
+                "100.0% Gastos de la categoria: prueba\n" +
+                "0.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 10.0%\n" +
+                "0.0% Ingresos de la categoria: prueba\n" +
+                "100.0% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2024-12-01T01:00 - Fecha fin: 2025-01-01T01:00\n" +
+                "% Gastos del mes: 7.8431377%\n" +
+                "0.0% Gastos de la categoria: prueba\n" +
+                "100.0% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 7.5000005%\n" +
+                "33.333336% Ingresos de la categoria: prueba\n" +
+                "66.66667% Ingresos de la categoria: muestra\n" +
+                "Fecha inicio: 2025-01-01T01:00 - Fecha fin: 2025-02-01T01:00\n" +
+                "% Gastos del mes: 0.0%\n" +
+                "NaN% Gastos de la categoria: prueba\n" +
+                "NaN% Gastos de la categoria: muestra\n" +
+                "% Ingresos del mes: 0.0%\n" +
+                "NaN% Ingresos de la categoria: prueba\n" +
+                "NaN% Ingresos de la categoria: muestra\n";
+        assertEquals(mensaje, factura);
 
     }
 
